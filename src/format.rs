455ddb7e8c3b6e0744652f8de8dd5bba856c34c3
Sede Soukossi <4968379+styvane@users.noreply.github.com>
2025-09-21 00:47:54 +0200

    Added trace formats

---
+++ src/format.rs
@@ -0,0 +1,80 @@
+//! This module defines the trace formatting styles
+
+use std::fmt;
+use std::str::FromStr;
+
+/// [`TraceFormat`]
+#[non_exhaustive]
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
+pub enum TraceFormat {
+    /// Compact traces
+    Compact,
+    /// Emits full verbose traces
+    Full,
+    /// Prettier traces
+    #[default]
+    Pretty,
+}
+
+impl fmt::Display for TraceFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let value = match self {
+            Self::Compact => "compact",
+            Self::Full => "full",
+            Self::Pretty => "pretty",
+        };
+        write!(f, "{value}")
+    }
+}
+
+impl FromStr for TraceFormat {
+    type Err = String;
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let trace_fmt = match value {
+            "compact" => Self::Compact,
+            "full" => Self::Full,
+            "pretty" => Self::Pretty,
+            _ => return Err("invalid trace format".into()),
+        };
+        Ok(trace_fmt)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use googletest::matchers::{anything, eq, err, ok};
+    use googletest::{assert_that, gtest};
+    use proptest::proptest;
+    use proptest::strategy::Strategy;
+    use rstest::rstest;
+
+    use super::TraceFormat;
+
+    #[rstest]
+    #[case(TraceFormat::Compact, "compact")]
+    #[case(TraceFormat::Full, "full")]
+    #[case(TraceFormat::Pretty, "pretty")]
+    fn display_correct_trace_format(#[case] trace_format: TraceFormat, #[case] display: &str) {
+        assert_that!(trace_format.to_string(), eq(display))
+    }
+
+    proptest! {
+        #[test]
+        fn parse_valid_trace_format_successfully(fmt in "compact|full|pretty") {
+            let result: Result<TraceFormat,_> = fmt.parse();
+            assert_that!(result, ok(anything()))
+        }
+    }
+
+    proptest! {
+        #[gtest]
+        fn parsing_invalid_trace_format_fails(
+            fmt in "[a-zA-Z]"
+            .prop_filter("Values must not be in enumerated values",
+                |fmt| !["compact", "full", "pretty"].contains(&fmt.as_str()))) {
+                let result: Result<TraceFormat, _> = fmt.parse();
+                assert_that!(result, err(anything()))
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! # Owiwi
+//!
+//! This crate define opinionated Opentelemetry tracer and metric initialization.
+pub mod format;
